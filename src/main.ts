b99d8e1fafdb936329fd525699020423046a56da
Haronkar <40798901+haronkar@users.noreply.github.com>
2024-12-09 15:05:00 -0800

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import "./style.css";
+
+const container: string[] = ["A"];
+container.push("B");
+
+const value = container.pop();
+
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+  <div>
+  ${container.length == 0}
+  </div>
+  <div>
+  ${value}
+  </div>
+`;
