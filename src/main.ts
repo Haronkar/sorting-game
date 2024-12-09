f833699b42d82007104ec0f1cf787e6c04fc73d0
Haronkar <40798901+haronkar@users.noreply.github.com>
2024-12-09 15:59:49 -0800

    testing stacks

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,49 @@
 import "./style.css";
 
-const container: string[] = ["A"];
-container.push("B");
+class Stack {
+  stack: string[];
+  constructor() {
+    this.stack = [];
+  }
+  push(element: string) {
+    this.stack.push(element);
+  }
+  pop() {
+    if (this.stack.length === 0) return "underflow";
+    return this.stack.pop();
+  }
+  isEmpty() {
+    return this.stack.length == 0;
+  }
+  printStack() {
+    let str = "";
+    for (const i of this.stack) str += i + " ";
+    return `${str}`;
+  }
+}
 
-const value = container.pop();
+function handleClick() {
+  // value = container.pop();
+  console.log("BAJHSBD");
+}
+
+let value: string | undefined = "None";
+
+const container = new Stack();
+container.push("B");
+container.push("C");
+container.push("A");
+container.push("D");
+container.push("F");
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+  <div onClick="${handleClick()}">
+  ${container.isEmpty()}
+  </div>
   <div>
-  ${container.length == 0}
+  ${container.printStack()}
+  </div>
+  <div>
+  ${value}
   </div>
 `;
